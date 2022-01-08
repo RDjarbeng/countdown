bc22c4627348703a92cdc0e0d3b64ed1b176afc1
Rdjarbeng <johndoe@example.com>
2022-01-08 20:04:37 +0000

    added offline functionality

--- sw.js
+++ sw.js
@@ -30,4 +30,9 @@ self.addEventListener('activate', evt=>{
 
 self.addEventListener('fetch', evt =>{
     // console.log('fetch event', evt);
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes=>{
+            return cacheRes|| fetch(evt.request);
+        })
+    )
 })
