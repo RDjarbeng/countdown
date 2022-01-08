8b17ea1d6971073c98e50647cf8b78b3bb77e6d6
Rdjarbeng <johndoe@example.com>
2022-01-08 19:53:50 +0000

    cached core files in service worker

--- sw.js
+++ sw.js
@@ -1,3 +1,33 @@
+const staticCacheName = 'site-static-av1'
+//assets to cache
+const assets =[
+    '/',
+    '/index.html',
+    '/authors.html',
+    '/app.js',
+    '/clock.js',
+    '/img/bg.svg',
+    '/styles.css',
+    'img/icons/favicon.png',
+    "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css",
+
+]
 self.addEventListener('install', evt=>{
-    console.log('service worker installed');
+    // console.log('service worker installed');
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache=>{
+        console.log('caching');
+        cache.addAll(assets);
+        })
+    )
+    
+})
+
+self.addEventListener('activate', evt=>{
+
+})
+
+self.addEventListener('fetch', evt =>{
+    // console.log('fetch event', evt);
 })
