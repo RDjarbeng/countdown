465e04638bcd2303633eb8c84994e15e6b635c60
Rdjarbeng <johndoe@example.com>
2022-01-08 18:54:50 +0000

    registered service worker,add theme color to .html

--- app.js
+++ app.js
@@ -112,7 +112,13 @@ autoLight();
 // init events
 icon.addEventListener("click", setMode);
 icon.addEventListener("click", notifyMode);
-console.log(myclock);
 //Prefer this 
 // startButton.addEventListener("click", restartTime);
 // endButton.addEventListener("click", stopClock);
+
+// service worker
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('/sw.js')
+        .then( (reg)=> console.log('service worker registered', reg))
+        .catch((err)=> console.log('Service worker not registered', err))
+}
---
+++ sw.js
@@ -0,0 +1,3 @@
+self.addEventListener('install', evt=>{
+    console.log('service worker installed');
+})
