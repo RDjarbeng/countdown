76fad45356e03d9dd3aafd45d32992d4870f463b
Nathaniel <73431750+nyakotey@users.noreply.github.com>
2022-01-23 15:37:46 +0000

    remove about sidebar option edit purple theme in dark mode

--- sidebar.js
+++ sidebar.js
@@ -1,5 +1,3 @@
-// import setMode from "./app.js";
-
 const $ = (selector) => document.querySelectorAll(selector);
 const featureNotReady = () => {
     window.location.href = "./fallback.html";
@@ -8,25 +6,16 @@ const nav = $(".nav")[0];
 const sidebar = $(".sidebar")[0];
 const sidebarItems = $(".sidebar-list-items");
 const colorIcons = $(".pick-color-ico");
-// $(".sidebar-close")[0].onclick = () => {
-//     sidebar.classList.replace("sidebar-show","sidebar-hide");
-// };
 nav.addEventListener("click", () => {
     if (sidebar.classList.contains("sidebar-hide")) {
-        // sidebar.classList.remove("sidebar-hide");
-        // sidebar.classList.add("sidebar-show");
         sidebar.classList.replace("sidebar-hide", "sidebar-show");
     }
-    // else{sidebar.classList.contains("sidebar-show")}
 });
 sidebarItems[0].onclick = featureNotReady;
 sidebarItems[1].onclick = featureNotReady;
 sidebarItems[2].onclick = openColorPicker;
 sidebarItems[3].onclick = () => {
     window.location.href = "./authors.html";
-};
-sidebarItems[4].onclick = () => {
-    alert("Countdown timer with dark and light mode, website");
 };
 
 function setTheme(event) {
@@ -43,8 +32,6 @@ colorIcons.forEach((e) => {
         setTheme(e);
     });
 });
-
-// todo: clicking outside sidebar hides it
 
 nav.addEventListener("click",(e)=>{
     $(".container")[0].addEventListener("click",closeSideBarListener);
