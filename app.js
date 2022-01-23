3145783a8cc4f6ec5cf23d7f302ef72ce1babfaf
Nathaniel <73431750+nyakotey@users.noreply.github.com>
2022-01-23 20:03:30 +0000

    add float button for new entry - hook for form inputs edit gap between banner row and clock

--- app.js
+++ app.js
@@ -113,11 +113,11 @@ autoLight();
 icon.addEventListener("click", setMode);
 icon.addEventListener("click", notifyMode);
 // service worker
-if('serviceWorker' in navigator){
-    window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-    .then( (reg)=> console.log('service worker registered', reg))
-        .catch((err)=> console.log('Service worker not registered', err));
-  });
+// if('serviceWorker' in navigator){
+//     window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/sw.js')
+//     .then( (reg)=> console.log('service worker registered', reg))
+//         .catch((err)=> console.log('Service worker not registered', err));
+//   });
         
-}
+// }
