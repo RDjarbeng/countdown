e5f42e73283e8601c8b3240e860fd4bd8d1ac1df
Rdjarbeng <johndoe@example.com>
2022-01-26 16:30:50 +0000

    fixed minimum date issue

--- form.js
+++ form.js
@@ -9,7 +9,7 @@ const popFormHtml = `<section class="pop-up-container">
     </div>
     <div>
         <label for="">Date &nbsp;</label>
-        <input type="date">
+        <input type="date" id ="dateInput" min="">
     </div>
     <div>
         <label for=""></label>
@@ -24,7 +24,24 @@ createButton.addEventListener("click", popForm);
 function popForm() {
     document.body.insertAdjacentHTML("afterbegin", popFormHtml);
     document.body.style.position = "fixed";
+    setDateAttributes();
     document.getElementsByClassName("close-form")[0].onclick = (e)=> {
         document.getElementsByClassName("pop-up-container")[0].remove();
     }
+}
+
+function setDateAttributes(){
+    const today = new Date();
+let dd = today.getDate()+1;//add 1 to the date so date starts from tomorrow
+let mm = today.getMonth()+1; //January is 0 so need to add 1 to make it 1!
+let yyyy = today.getFullYear();
+if(dd<10){
+  dd='0'+dd
+} 
+if(mm<10){
+  mm='0'+mm
+} 
+
+let todayString = yyyy+'-'+mm+'-'+dd;
+document.getElementById("dateInput").setAttribute("min", todayString);
 }
