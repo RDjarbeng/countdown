bfbe41cdafb2e2daf7a3c6a1901a4043e563a075
Rdjarbeng <johndoe@example.com>
2022-02-16 13:43:53 +0000

    add populateList function

--- displayCountdowns.js
+++ displayCountdowns.js
@@ -12,45 +12,56 @@ async function displayCountdowns() {
     let JsonListOfCountdowns = await localStorage.getItem('countdown');
     if (JsonListOfCountdowns) {
         let arrayOfCountdowns = JSON.parse(JsonListOfCountdowns).reverse();
-        let listItems = '';
-        arrayOfCountdowns.forEach((countdown, index) => {
-            let date = new Date(countdown.date);
-            let dateModified = new Date(countdown.dateModified)
-            listItems += `<div class="countdown-list-item"  data-index="${index}" data-id ="${countdown.dateModified}"style ="border-bottom: 0.1em solid blue; padding: 0.6em;">
-        <div class="countdown-list-text">
-         <span>Text: </span> ${countdown.text} </div>
-        <div>
-        <span>Date: </span>${date.getDate() + ' ' + date.toLocaleString('default', { month: 'long' }) + ', ' + date.getFullYear()}
-        </div>
-        <div>
-        <span>Date Modified: </span>${dateModified.getDate() + ' ' + dateModified.toLocaleString('default', { month: 'long' }) + ', ' + dateModified.getFullYear()}
-        </div>
-        </div>`
-        });
+        let listItems = populateList(arrayOfCountdowns);
+        
         countdownList.innerHTML = listItems;
+
+
         let clock = new Clock(new Date(arrayOfCountdowns[0].date));
         coundownTextDisplay.innerHTML = arrayOfCountdowns[0].text;
         // waitForAnimation(new Clock(new Date(arrayOfCountdowns[0].date)));
         // myclock.endDate = new Date(arrayOfCountdowns[0].date)
         stopClock();
         waitForAnimation(clock, { dayNumber, hourNumber, minNumber, secNumber })
+
+        document.querySelectorAll('.countdown-list-item').forEach(item => {
+            console.log(item, this);
+            
+            item.addEventListener('click', event => {
+                
+              console.log('running' ,item,item.getAttribute('data-index'), event.target);
+              console.log( arrayOfCountdowns);
+            })
+          })
     } else {
         countdownList.innerHTML = 'Found no countdowns to display';
     }
     // console.log(myClock);
 }
 
+function populateList(arrayOfCountdowns){
+    let listItems = '';
+    arrayOfCountdowns.forEach((countdown, index) => {
+        let date = new Date(countdown.date);
+        let dateModified = new Date(countdown.dateModified)
+        listItems += `<div class="countdown-list-item"  data-index="${index}" data-id ="${countdown.dateModified}"style ="border-bottom: 0.1em solid blue; padding: 0.6em;">
+    <div class="countdown-list-text">
+     <span>Text: </span> ${countdown.text} </div>
+    <div>
+    <span>Date: </span>${date.getDate() + ' ' + date.toLocaleString('default', { month: 'long' }) + ', ' + date.getFullYear()}
+    </div>
+    <div>
+    <span>Date Modified: </span>${dateModified.getDate() + ' ' + dateModified.toLocaleString('default', { month: 'long' }) + ', ' + dateModified.getFullYear()}
+    </div>
+    </div>`
+    });
+    return listItems;
+}
+
 
 
 await displayCountdowns();
-document.querySelectorAll('.countdown-list-item').forEach(item => {
-    console.log(item, this);
-    
-    item.addEventListener('click', event => {
-        
-      console.log('running' ,item,item.getAttribute('data-index'), event.target);
-    })
-  })
+
 
   function test (){
       console.log(this);
