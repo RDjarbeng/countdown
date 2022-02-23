f3be34921d86613de320927c28a44e4f54f3833b
Rdjarbeng <johndoe@example.com>
2022-02-23 23:28:48 +0000

    added event listener to the list container in countdown list

--- displayCountdowns.js
+++ displayCountdowns.js
@@ -8,7 +8,7 @@ const secNumber = document.getElementById("sec-num");
 const coundownTextDisplay = document.getElementById('countdown-text')
 // todo: sort by modified time
 async function displayCountdowns() {
-    
+
     let countdownList = document.getElementById('countdown-list');
 
     let JsonListOfCountdowns = await localStorage.getItem('countdown');
@@ -17,15 +17,25 @@ async function displayCountdowns() {
         let listItems = populateList(arrayOfCountdowns);
         countdownList.innerHTML = listItems;
         updateClockAndText(arrayOfCountdowns[0].date, arrayOfCountdowns[0].text)
-        document.querySelectorAll('.countdown-list-text').forEach(item => {
+        document.querySelectorAll('.countdown-list').forEach(item => {
             item.addEventListener('click', event => {
-                // todo: find a better way of accessing element in countdown array
-                if([null,"",undefined].includes(document.querySelector(".clock-row").style.display)){
-                    document.getElementsByClassName("clock-row")[0].style.display="flex";
-                    document.getElementsByClassName("clock-row")[0].style.animationPlayState="running";
+                const targetElement = event.target;
+                console.log(targetElement.className);
+                // if event is fired on text or date
+                if (targetElement.className == 'countdown-list-text' || targetElement.className == 'countdown-list-date') {
+                    console.log('clicking within the text');
+                    // todo: find a better way of accessing element in countdown array
+                    updateClockAndText(arrayOfCountdowns[targetElement.parentElement.getAttribute('data-index')].date, arrayOfCountdowns[targetElement.parentElement.getAttribute('data-index')].text)
                     
+                    if ([null, "", undefined].includes(document.querySelector(".clock-row").style.display)) {
+                        document.querySelector(".clock-row").style.display = "flex";
+                        document.querySelector(".clock-row").style.animationPlayState = "running";
+
+                    }
+
                 }
-                updateClockAndText(arrayOfCountdowns[item.parentElement.getAttribute('data-index')].date, arrayOfCountdowns[item.parentElement.getAttribute('data-index')].text)
+
+                // updateClockAndText(arrayOfCountdowns[item.parentElement.getAttribute('data-index')].date, arrayOfCountdowns[item.parentElement.getAttribute('data-index')].text)
                 // console.log('running', item, item.getAttribute('data-index'), event.target);
             })
         })
@@ -59,22 +69,24 @@ function updateClockAndText(date, text, animation = true) {
     let clock = new Clock(new Date(date));
     coundownTextDisplay.innerHTML = text;
     stopClock();
-    waitForAnimation(clock, { dayNumber, hourNumber, minNumber, secNumber },500)
+    waitForAnimation(clock, { dayNumber, hourNumber, minNumber, secNumber }, 500)
 }
 
 
-await displayCountdowns();
 
-const ContextMenu=(event)=>{
-    if(event.currentTarget.querySelector(".menu").style.display == "block"){
+
+const ContextMenu = (event) => {
+    if (event.currentTarget.querySelector(".menu").style.display == "block") {
         event.currentTarget.querySelector(".menu").style.display = "none";
         console.log("context-menu: hide");
     }
-    else{
+    else {
         event.currentTarget.querySelector(".menu").style.display = "block";
         console.log("context-menu: show");
     }
 }
-document.querySelectorAll(".countdown-list-options").forEach((option)=>{
-    option.addEventListener("click",ContextMenu);
+document.querySelectorAll(".countdown-list-options").forEach((option) => {
+    option.addEventListener("click", ContextMenu);
 });
+
+await displayCountdowns();
