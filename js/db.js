a00635696e084cfe35bdab7074092a7722c721a8
Rdjarbeng <johndoe@example.com>
2022-04-13 21:16:06 +0000

    add firebase config info

---
+++ js/db.js
@@ -0,0 +1,8 @@
+import { collection, getDocs } from "firebase/firestore"; 
+// real time listener
+// db.collection('countdown').onSnapshot((snapshot)=>{
+//     console.log(snapshot.docChanges());
+// })
+
+
+
