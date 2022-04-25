dde4ff7a165d21753f9c23e595554fafa959fd7d
Nathaniel <73431750+nyakotey@users.noreply.github.com>
2022-04-25 21:15:03 +0000

    change about page icon in sidebar activate service worker

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // #5
 
-const staticCacheName = 'site-static-v35';
-const dynamicCache = 'site-dynamic-v35';
+const staticCacheName = 'site-static-v36';
+const dynamicCache = 'site-dynamic-v36';
 
 const dynamicCacheSize = 30;
 
