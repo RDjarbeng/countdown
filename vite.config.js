729c8fd497d949865ff910d0c26a15bc207e8bb0
Rdjarbeng <johndoe@example.com>
2022-05-23 20:53:43 +0000

    get vite running

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,38 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
-// import legacy from '@vitejs/plugin-legacy';
+// import { resolve } from 'path';
+// import { defineConfig } from 'vite';
+// // import legacy from '@vitejs/plugin-legacy';
 
-export default defineConfig({
-  plugins: [
-    legacy({
-      targets: ['defaults', 'not IE 11'],
-    }),
-  ],
+// export default defineConfig({
+//   plugins: [
+//     // legacy({
+//     //   targets: ['defaults', 'not IE 11'],
+//     // }),
+//   ],
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         main: resolve(__dirname, 'index.html'),
+//         about: resolve(__dirname, './html/about.html'),
+//         countdownList: resolve(__dirname, './html/countdown-list.html'),
+//         fallback: resolve(__dirname, './html/fallback.html'),
+//         today: resolve(__dirname, './html/today.html'),
+
+//         // aboutjs: resolve(__dirname, 'about.js')
+//       },
+//     },
+//   },
+// });
+
+const { resolve } = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        about: resolve(__dirname, './html/about.html'),
-        countdownList: resolve(__dirname, './html/countdown-list.html'),
-        fallback: resolve(__dirname, './html/fallback.html'),
-        today: resolve(__dirname, './html/today.html'),
-
-        // aboutjs: resolve(__dirname, 'about.js')
-      },
-    },
-  },
-});
+        nested: resolve(__dirname, 'nested/index.html')
+      }
+    }
+  }
+})
