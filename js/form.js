8494a6869bbcc49f2b7e4641bd7c3bf64d424a44
Rdjarbeng <johndoe@example.com>
2022-05-25 11:18:20 +0000

    change pop form function and separate display from functionality

--- js/form.js
+++ js/form.js
@@ -1,42 +1,10 @@
-export function popForm() {
-    countNumber = localStorage.getItem('countNumber');
-    if (!countNumber)
-        countNumber = 1;
-    const popFormHtml = `<section class="pop-up-container">
-    <form id='customDateForm' class="pop-up-form">
-        <div class="form-header">Set Countdown</div>
-        <div class="form-sections">
-            <label for="">Title &nbsp;</label>
-            <input type="text" placeholder="countdown #${countNumber}" id='countdownText'>
-        </div>
-        <div class="form-sections">
-            <label for="">Date & Time &nbsp;</label>
-            <input type="datetime-local" id ="dateInput" min="" required>
-        </div>
-        <div class="form-sections form-repeat">
-            <label for="repeat-cb">
-                <input type="checkbox" id="repeat-cb"> Repeat every year 
-            </label>
-        </div>
-        <div class="form-sections">
-            <label for=""></label>
-            <input type="submit" id ="countdown-submit"value="Submit">
-        </div>    
-        <div class="close-form"><button>Close</button></div>
-    </form>
-    </section>`;
+import {sanitize} from './functions.js'
+import {popForm, closeFormPopUp} from './formfunctions.js'
 
-    document.body.insertAdjacentHTML("afterbegin", popFormHtml);
-    document.body.style.position = "fixed";
-    setDateAttributes();
+const popAndAddFormSubmission=()=>{
+    popForm()
     handleFormSubmission();
-    document.getElementsByClassName("close-form")[0].onclick = (e) => { 
-        // console.log('close firing');
-        closeFormPopUp();
-     }
-    
 }
-
 function addZeros(time) {
     if (time < 10) {
         time = "0" + time;
@@ -44,40 +12,10 @@ function addZeros(time) {
     return time;
 }
 
-function setDateAttributes() {
-    const dateInput = document.getElementById("dateInput");
-    const today = new Date();
-    let dd = today.getDate();//add 1 to the date so date starts from tomorrow
-    let mm = today.getMonth() + 1; //January is 0 so need to add 1 to make it 1!
-    let yyyy = today.getFullYear();
-    let hr = addZeros(today.getHours());
-    let min = addZeros(today.getMinutes());
-    dd = addZeros(dd);
-    mm = addZeros(mm)
 
-    let todayString = yyyy + '-' + mm + '-' + dd + 'T' + hr + ':' + min;
-    console.log(todayString);
-    dateInput.setAttribute("min", todayString);
-    dateInput.value = todayString;
-}
 
-export function closeFormPopUp() {
-    document.getElementsByClassName("pop-up-container")[0].remove();
-    document.body.style.position = "";
-}
 
-export function sanitize(string) {
-    const map = {
-        '&': '&amp;',
-        '<': '&lt;',
-        '>': '&gt;',
-        '"': '&quot;',
-        "'": '&#x27;',
-        "/": '&#x2F;',
-    };
-    const reg = /[&<>"'/]/ig;
-    return string.replace(reg, (match) => (map[match]));
-}
+
 
 function checkRepeat(repeatCheckBox) {
     if (repeatCheckBox.checked) {
@@ -96,7 +34,7 @@ function handleFormSubmission() {
         let userDate = document.getElementById("dateInput").value;
         let repeatCheck = document.getElementById("repeat-cb");
         let userTextField = document.getElementById('countdownText');
-        // submitbutton.disabled = true;
+        submitbutton.disabled = true;
         // get text field values, with auto values
 
         let userText = sanitize(userTextField.value);
@@ -155,4 +93,4 @@ if (!document.querySelector("[href='css/form.css']")) {
 
 
 
-createButton.addEventListener("click", popForm);
+createButton.addEventListener("click", popAndAddFormSubmission);
