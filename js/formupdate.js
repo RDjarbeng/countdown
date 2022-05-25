e80fb3cec18797f3497497927d6301305a4680b7
Rdjarbeng <johndoe@example.com>
2022-05-25 09:55:40 +0000

    import functions from form.js remove duplicate functions

--- js/formupdate.js
+++ js/formupdate.js
@@ -1,37 +1,5 @@
-function popForm() {
-    countNumber = localStorage.getItem('countNumber');
-    if (!countNumber)
-        countNumber = 1;
-    const popFormHtml = `<section class="pop-up-container">
-    <form action="/html/countdown-list.html" method="get" id='customDateForm' class="pop-up-form">
-        <div class="form-header">Set Countdown</div>
-        <div class="form-sections">
-            <label for="">Title &nbsp;</label>
-            <input type="text" placeholder="countdown #${countNumber}" id='countdownText'>
-        </div>
-        <div class="form-sections">
-            <label for="">Date & Time &nbsp;</label>
-            <input type="datetime-local" id ="dateInput" min="" required>
-        </div>
-        <div class="form-sections form-repeat">
-            <label for="repeat-cb">
-            <input type="checkbox" id="repeat-cb"> Repeat every year 
-            </label>
-        </div>
-        <div class="form-sections">
-            <label for=""></label>
-            <input type="submit" id ="countdown-submit"value="Submit">
-        </div>    
-        <div class="close-form"><button>Close</button></div>
-    </form>
-    </section>`;
+import {popForm} from './form.js'
 
-    document.body.insertAdjacentHTML("afterbegin", popFormHtml);
-    document.body.style.position = "fixed";
-    setDateAttributes();
-    document.getElementsByClassName("close-form")[0].onclick = (e) => { closeFormPopUp(); }
-    handleFormSubmission();
-}
 
 function addZeros(time) {
     if (time < 10) {
