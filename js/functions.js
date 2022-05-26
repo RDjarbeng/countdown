713c663347856b630537d3b02184b4f9038e01bb
Rdjarbeng <johndoe@example.com>
2022-05-26 23:11:34 +0000

    move populate list, set countdown status, and getCountdownstring

--- js/functions.js
+++ js/functions.js
@@ -91,7 +91,58 @@ export function updateLocalItem(arrayOfCountdowns, countItem, id) {
 
 }
 
+/**
+ * Returns html string with a list of countdowns
+ * @param {Array.<{text: String, date: String, dateModified: String, repeat: String}>} arrayOfCountdowns | contains array of countdown objects
+ * @returns {string} list of countdownitems to be appended to DOM
+ */
+ export function populateList(arrayOfCountdowns) {
+    let listItems = '';
+    sortArrayOnSelection(arrayOfCountdowns);
+    arrayOfCountdowns.forEach((countdown, index) => {
+        listItems += addCountdownItem(countdown, index)
+    });
+    return listItems;
+}
+/**
+ * 
+ * @param {Clock} clock 
+ * @returns {object} status
+ * @returns {object} status.countdownStatus
+ * @returns {object} status.countdownStatusTI
+ */
+export function setCountDownStatus(clock){
+    let timeDifference = clock.getDistance();
+    let countdownStatus = "";
+    let countdownStatusTI = `<span style="color:#03bf42;"><i class="fas fa-hourglass-start"></i> active</span>`;
+    if (timeDifference > 0) {
+        countdownStatus = getCountdownString(clock);
+    } else {
+        // countdown elapsed
+        countdownStatus = 'Due: ' + clock.endDate.getDate() + ' ' + clock.endDate.toLocaleString('default', { month: 'long' }) + ', ' + clock.endDate.getFullYear();
+        countdownStatusTI = `<span style="color:crimson;"><i class="fas fa-hourglass-end"></i> elapsed</span>`;
+    }
+    return {countdownStatus, countdownStatusTI}
+}
 
+/**
+ * Get string with status of countdowns
+ * @param {Clock} clock clock object for particular countdown
+ * @returns {String} string of countdown status
+ */
+ export function getCountdownString(clock) {
+    let countdownString = '';
+    if (clock.days > 0) {
+        countdownString = clock.days + ' days, ' + ((clock.hours > 0) ? (clock.hours + ' hours') : (clock.minutes + ' minutes'));
+    } else if (clock.hours > 0) {
+        countdownString = clock.hours + ' hours, ' + ((clock.minutes > 0) ? (clock.minutes + ' minutes') : (clock.seconds + ' seconds'));
+    } else if (clock.minutes > 0) {
+        countdownString = clock.minutes + ' minutes, ' + clock.seconds + ' seconds';
+    } else if (clock.seconds >= 0) {
+        countdownString = clock.seconds + ' seconds '
+    }
+    return ` ${countdownString} more`
+}
 
 /**
  * Sort countdown array by date modified or due date
