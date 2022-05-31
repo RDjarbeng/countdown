0544a393bc1f520e766ad0aa6d7dbf9949372808
Rdjarbeng <johndoe@example.com>
2022-05-31 22:20:54 +0000

    remove unused imports and vars

--- js/displayCountdowns.js
+++ js/displayCountdowns.js
@@ -1,10 +1,7 @@
-import { Clock } from "./clock.js";
-import { setInnerHtmlForNotNull } from "./functions.js";
-import { getCountdownString, populateList, getCountItemExists, setCountItemExists, setCountItemStatus, fetchArrayOfCountdowns,   LISTPAGE_DOM_IDS, displayCountdowns, addEventHandlers, displayAndStartcount, displayAndAddListeners } from "./listFunctions.js";
+import { displayAndAddListeners } from "./listFunctions.js";
 import { errorHandler } from "./error.js";
 // Dom elements
 // begin displaycountdown.js
-let arrayOfCountdowns = fetchArrayOfCountdowns();
 
 
 
