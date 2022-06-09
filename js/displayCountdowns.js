ac4273bee535900bca879888bf168ac4df82796e
Rdjarbeng <johndoe@example.com>
2022-06-09 21:02:58 +0000

    shorten display countdowns using loadListpage()

--- js/displayCountdowns.js
+++ js/displayCountdowns.js
@@ -1,12 +1,3 @@
-import { displayAndAddListeners } from "./listFunctions.js";
-import { errorHandler } from "./error.js";
-// Dom elements
+import { loadListPage } from "./listFunctions.js";
 // begin displaycountdown.js
-
-try {
-    displayAndAddListeners();
-} catch (err) {
-    console.log(err, 'err in display countdown initialisation');
-    errorHandler("Unable to fetch your countdowns");
-}
-
+loadListPage();
