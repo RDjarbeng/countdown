c14cc765fd11e0b9994e337e5ccb73b613ad1f84
RDjarbeng <richarddjarbeng@gmail.com>
2022-12-25 16:37:35 +0200

    clock ui functions, update imports

---
+++ js/list_ui/updateListpageClockAndText.js
@@ -0,0 +1,26 @@
+import { removeElementSetDisplayNone, setInnerHtmlForNotNull, stopClock } from "./functions.js";
+import { countdownTextDisplay, countdownClock } from "./listFunctions";
+
+
+
+export function removeClockAndText() {
+    stopClock();
+    setInnerHtmlForNotNull(countdownTextDisplay, '');
+    if (countdownClock) {
+        removeElementSetDisplayNone(countdownClock);
+    }
+}
+
+/**
+ * Display the mini clock on the countdownlist page
+ */
+ export const showClockRow = () => {
+    //  if ([null, "", undefined].includes(document.querySelector(".clock-row").style.display)) {
+    const clockRow = document.querySelector(".clock-row");
+    if (clockRow) {
+        console.log('Showing the clock row');
+        clockRow.style.display = "flex";
+        clockRow.style.animationPlayState = "running";
+    }
+    // }
+};
