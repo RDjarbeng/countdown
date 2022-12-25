ff50db82f716821e4b5655bfb73bed93981d5ea2
RDjarbeng <richarddjarbeng@gmail.com>
2022-12-25 16:31:45 +0200

    Move listpage element ids to different file

---
+++ js/LISTPAGE_DOM_IDS.js
@@ -0,0 +1,9 @@
+/* SECTION: DISPLAY COUNTDOWNS */
+export const LISTPAGE_DOM_IDS = {
+    clockDayElement: 'day-num',
+    clockHourElement: 'hour-num',
+    clockMinuteElement: 'min-num',
+    clockSecondElement: 'sec-num',
+    countdownTextDisplay: 'countdown-text',
+    countdownList: 'countdown-list'
+};
