e479eb36ff57f9420b9d5546de0c376af37534dd
RDjarbeng <richarddjarbeng@gmail.com>
2022-12-27 17:23:23 +0200

    use addSortUI instead, without duplicate listeners

--- js/displayCountdowns.js
+++ js/displayCountdowns.js
@@ -2,6 +2,7 @@ import { popAndAddFormOnList } from "./formupdate.js";
 import { addEventHandlers } from "./listpage/listEventListener.js";
 import { loadListPage } from "./listpage/listFunctions.js";
 import { addSortUIAndListeners } from "./listpage/list_sort/sort.js";
+import { addSortUI } from "./listpage/list_ui/addSortUI.js";
 // import { addEventHandlers } from "./listEventListener";
 
 // begin displaycountdown.js
@@ -11,8 +12,8 @@ addSortUIAndListeners()
 const handleSubmissionAndLoadListpage=()=>{
     popAndAddFormOnList();
     loadListPage();
-addEventHandlers();
-addSortUIAndListeners()
+// addEventHandlers();
+addSortUI();
 }
 //get button for create countdown
 const createButton = document.getElementsByClassName("new-item")[0];
