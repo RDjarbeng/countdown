d79043a89ca2bd5d82e27c2777a5a661cef072ec
RDjarbeng <richarddjarbeng@gmail.com>
2022-12-27 14:10:06 +0200

    add listener function to refresh page after form submit

--- js/displayCountdowns.js
+++ js/displayCountdowns.js
@@ -1,3 +1,12 @@
+import { popAndAddFormOnList } from "./formupdate.js";
 import { loadListPage } from "./listpage/listFunctions.js";
 // begin displaycountdown.js
 loadListPage();
+
+const handleSubmissionAndLoadListpage=()=>{
+    popAndAddFormOnList();
+    loadListPage();
+}
+//get button for create countdown
+const createButton = document.getElementsByClassName("new-item")[0];
+createButton.addEventListener("click", handleSubmissionAndLoadListpage);
