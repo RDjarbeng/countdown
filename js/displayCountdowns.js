21bc4c31794ad87ccb2d0454a5726eb6dc0d2f44
RDjarbeng <richarddjarbeng@gmail.com>
2022-12-27 16:59:39 +0200

    addEvent handlers and sortui to reregister on form submission

--- js/displayCountdowns.js
+++ js/displayCountdowns.js
@@ -1,11 +1,18 @@
 import { popAndAddFormOnList } from "./formupdate.js";
+import { addEventHandlers } from "./listpage/listEventListener.js";
 import { loadListPage } from "./listpage/listFunctions.js";
+import { addSortUIAndListeners } from "./listpage/list_sort/sort.js";
+// import { addEventHandlers } from "./listEventListener";
+
 // begin displaycountdown.js
 loadListPage();
-
+addEventHandlers();
+addSortUIAndListeners()
 const handleSubmissionAndLoadListpage=()=>{
     popAndAddFormOnList();
     loadListPage();
+addEventHandlers();
+addSortUIAndListeners()
 }
 //get button for create countdown
 const createButton = document.getElementsByClassName("new-item")[0];
