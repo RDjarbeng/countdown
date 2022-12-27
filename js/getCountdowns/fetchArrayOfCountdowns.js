54ccc8974924b1344a67db3c1cc42d77d5aba42d
RDjarbeng <richarddjarbeng@gmail.com>
2022-12-27 14:13:38 +0200

    move fetchArrayOfCountdowns to listfunctions

--- js/getCountdowns/fetchArrayOfCountdowns.js
+++ js/getCountdowns/fetchArrayOfCountdowns.js
@@ -1,13 +1,1 @@
-/**
- *
- * @returns {Array} Array of countdowns stored in local storage, null if none
- */
-function fetchArrayOfCountdowns() {
-    let jsonListOfCountdowns = localStorage.getItem('countdown');
-    if (!jsonListOfCountdowns) {
-        console.warn('Unable to fetch your saved countdowns in fetch array()');
-        console.log('Warning in fetch Array of Countdowns, null fetched');
-        return null;
-    }
-    return JSON.parse(jsonListOfCountdowns);
-}
+
--- js/listpage/listFunctions.js
+++ js/listpage/listFunctions.js
@@ -272,6 +272,19 @@ export function loadListPage(){
     }
 }
 
+/**
+ *
+ * @returns {Array} Array of countdowns stored in local storage, null if none
+ */
+ function fetchArrayOfCountdowns() {
+    let jsonListOfCountdowns = localStorage.getItem('countdown');
+    if (!jsonListOfCountdowns) {
+        console.warn('Unable to fetch your saved countdowns in fetch array()');
+        console.log('Warning in fetch Array of Countdowns, null fetched');
+        return null;
+    }
+    return JSON.parse(jsonListOfCountdowns);
+}
 
 export const getArrayOfCountdownStatus=()=>{
     if(!arrayOfCountdowns){
