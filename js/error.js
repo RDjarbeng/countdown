438f1ce046c9a309109d9e0aa467e9a8edf163e2
Nathaniel <73431750+nyakotey@users.noreply.github.com>
2023-01-05 22:28:53 +0000

    add notification types: info and error

--- js/error.js
+++ js/error.js
@@ -1,3 +1,5 @@
+import "../css/notification.css";
+import { $ } from "./functions.js";
 /**
  * remove div containing error message
  * @param {Event} event 
@@ -18,61 +20,24 @@ export const errorHandlerWithoutMessage = (err) => {
 export const errorHandler = (msg) => {
     const errMessage = "Oops an error occurred 🤧😐";
     let errHtml = `
-    <section class="error-notification">
-    <style>
-        .error-notification {
-            width: 100vw;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            background: transparent;
-            z-index: 99;
-            position: absolute;
-            padding-top: 10vmin;
-        }
-
-        .error-message {
-            width: 80vmin;
-            padding: 2vmin;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            border-radius: 2vmin;
-            background: #f8d7da;
-            color: #842029;
-            position: relative;
-        }
-
-        .error-close {
-            position: absolute;
-            right: 3%;
-            padding: 1.2vmin 2.2vmin;
-        }
-        @media screen and (max-width: 427px) {
-            .error-message{
-                width: 90vmin;
-                padding: 3.3vmin;
-            }
-        }
-    </style>
-    <div class="error-message">
-        <span>${msg || errMessage}</span>
-        <div class="error-close" >
-            <i class="fas fa-times"></i>
+    <section class="notification notification--error">
+        <div class="notification_msg">
+            <span>${msg || errMessage}</span>
+            <div class="notification_close" >
+                <i class="fas fa-times"></i>
+            </div>
         </div>
-    </div>
-</section>
-`;
+    </section>`;
+
     if (prevErr) {
-        let item = document.querySelectorAll(".error-notification")[0]
-        if (item)
-            item.remove();
+        let item = $(".notification--error")[0];
+        if (item) item.remove();
         document.body.insertAdjacentHTML("afterbegin", errHtml);
     } else {
         document.body.insertAdjacentHTML("afterbegin", errHtml);
         prevErr = true;
     }
-    document.querySelector('.error-message').addEventListener('click', closeErrorInfo)
+    document.querySelector('.notification_msg').addEventListener('click', closeErrorInfo)
 };
 let prevErr = false;
 window.onerror = errorHandlerWithoutMessage;
