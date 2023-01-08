d25ecbcc9e97c90ae518d6e5df6cb03398c9b18c
RDjarbeng <richarddjarbeng@gmail.com>
2023-01-08 15:48:44 +0200

    Move homepage DOM IDs to different file

--- app.js
+++ app.js
@@ -2,19 +2,11 @@ import { Clock, NewYearClock } from "./js/clock.js";
 import { animateAndStartClock } from "./js/appfunctions.js";
 import { errorHandler } from "./js/error.js";
 import { setInnerHtmlForNotNull } from "./js/functions.js";
+import { HOMEPAGE_DOM_IDS } from "./js/HOMEPAGE_DOM_IDS.js";
 // import registerUpdateServiceWorker from "./js/serviceWorkerUpdate"
 
 // DOM nodes
 const animatedCountDuration = 800;
-const HOMEPAGE_DOM_IDS ={
-    clockDayElement:'day-num',
-    clockHourElement: 'hour-num',
-    clockMinuteElement: 'min-num',
-    clockSecondElement: 'sec-num',
-    countdownTextDisplay: 'countdown-text',
-    dueDate: 'dueDate',
-}
-
 var dayNumber = document.getElementById(HOMEPAGE_DOM_IDS.clockDayElement);
 var hourNumber = document.getElementById(HOMEPAGE_DOM_IDS.clockHourElement);
 var minNumber = document.getElementById(HOMEPAGE_DOM_IDS.clockMinuteElement);
@@ -49,7 +41,8 @@ function setMainText(countdownText) {
 try {
     //show day value before animation runs
     animateAndStartClock(myclock, { dayNumber, hourNumber, minNumber, secNumber }, animatedCountDuration);
-
+    addEventListener('elapsed', countHasElapsedListener);
+    coun
     // addWhatappEventHandler();
     // as;
 } catch (error) {
---
+++ js/HOMEPAGE_DOM_IDS.js
@@ -0,0 +1,8 @@
+export const HOMEPAGE_DOM_IDS = {
+    clockDayElement: 'day-num',
+    clockHourElement: 'hour-num',
+    clockMinuteElement: 'min-num',
+    clockSecondElement: 'sec-num',
+    countdownTextDisplay: 'countdown-text',
+    dueDate: 'dueDate',
+};
