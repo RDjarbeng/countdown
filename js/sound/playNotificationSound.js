c8c8cb9565e60485c7690e7ac2a523a9d5dea5fc
RDjarbeng <richarddjarbeng@gmail.com>
2023-01-12 14:29:49 +0200

    change sound to notification3

--- js/sound/playNotificationSound.js
+++ js/sound/playNotificationSound.js
@@ -1,5 +1,5 @@
 export function playNotificationSound() {
-    let song = new Audio("notification1.mp3");
+    let song = new Audio("notification3.mp3");
     console.log(song);
     song.play();
 }
