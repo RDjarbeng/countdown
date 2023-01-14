a53c34546f2746e071d63cb57e8e8ba88458fa25
RDjarbeng <richarddjarbeng@gmail.com>
2023-01-14 11:54:59 +0200

    make days of year parameter variable, import NewYearClock

--- dayOfYear.js
+++ dayOfYear.js
@@ -3,6 +3,8 @@
 // {
 //     var head = document.getElementsByTagName('head')[0];
 
+import { NewYearClock } from "./js/clock";
+
 //     var script = document.createElement('script');
 //     script.src = filename;
 //     script.type = 'text/javascript';
@@ -35,9 +37,11 @@ class Clock {
         return this.dayLength - this.days
     }
 }
-let dayCount = new Clock().countDays();
-document.querySelector('div').innerText+= 'Day '+dayCount+'/365';
-let url = `whatsapp://send?text=Day ${dayCount || 'X'}/365`;
+const dayClock = new NewYearClock()
+let dayCount = dayClock.countDays();
+let daysInyear = dayClock.getDaysinYear();
+document.querySelector('div').innerText+= 'Day '+dayCount+'/6' +(daysInyear|'365');
+let url = `whatsapp://send?text=Day ${dayCount || 'X'}/${daysInyear|'365'}`;
 console.log('days', dayCount);
 console.log(window.location.protocol + "me //" + window.location.host );
 // window.location.replace(url);
