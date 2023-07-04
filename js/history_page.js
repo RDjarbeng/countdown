360c1af08cfb720cb4e8c61fcf16926c13d2be2a
RDjarbeng <richarddjarbeng@gmail.com>
2023-07-04 11:57:00 +0200

    add js file for  history page

---
+++ js/history_page.js
@@ -0,0 +1,24 @@
+import { popAndAddFormOnList } from "./formupdate.js";
+import { addEventHandlers } from "./listpage/listEventListener.js";
+import { loadListPage } from "./listpage/listFunctions.js";
+import { addSortUIAndListeners } from "./listpage/list_sort/sort.js";
+import { addSortUI } from "./listpage/list_ui/addSortUI.js";
+import { requestNotificationPermission } from "./notification";
+// import { addEventHandlers } from "./listEventListener";
+
+// begin displaycountdown.js
+loadListPage();
+addEventHandlers();
+addSortUIAndListeners()
+const handleSubmissionAndLoadListpage=()=>{
+    popAndAddFormOnList();
+    loadListPage();
+// addEventHandlers();
+addSortUI();
+}
+//get button for create countdown
+const createButton = document.getElementsByClassName("new-item")[0];
+createButton.addEventListener("click", handleSubmissionAndLoadListpage);
+if(Notification.permission !=='granted')
+requestNotificationPermission();
+
